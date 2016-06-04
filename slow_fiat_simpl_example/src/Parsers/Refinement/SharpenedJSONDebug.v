39ce72a50d8515b3041dc34359762ab3e8e666c8
Jason Gross <jgross@mit.edu>
2016-06-03 22:19:11 -0400

    Fix some simpl files

---
+++ slow_fiat_simpl_example/src/Parsers/Refinement/SharpenedJSONDebug.v
@@ -0,0 +1,21 @@
+(** Sharpened ADT for JSON *)
+Require Import Fiat.Parsers.Grammars.JSONImpoverished.
+Require Import Fiat.Parsers.Refinement.Tactics.
+Require Import Fiat.Parsers.Refinement.DisjointRules.
+Require Import Fiat.Parsers.Refinement.DisjointRulesRev.
+Require Import Fiat.Parsers.ExtrOcamlParsers. (* for simpl rules for [find_first_char_such_that] *)
+Require Import Fiat.Parsers.Refinement.BinOpBrackets.BinOpRules.
+Require Import Fiat.Parsers.StringLike.String.
+Require Import Fiat.Parsers.Refinement.SharpenedJSON.
+
+Require Export Fiat.Parsers.ParserFromParserADT.
+Require Export Fiat.Parsers.ExtrOcamlParsers.
+Export Fiat.Parsers.ExtrOcamlParsers.HideProofs.
+Require Export Fiat.Parsers.StringLike.OcamlString.
+
+Definition json_parser (str : Coq.Strings.String.string) : bool.
+Proof.
+  Start Profiling.
+  Time make_parser (@ComputationalSplitter(* _ String.string_stringlike _ _*)).
+  Show Profile.
+Time Defined.

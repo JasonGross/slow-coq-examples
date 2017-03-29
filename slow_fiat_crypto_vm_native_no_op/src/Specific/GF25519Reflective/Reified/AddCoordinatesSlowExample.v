08d88f354f9518e39a1861304d3642da1000710f
Jason Gross <jgross@mit.edu>
2017-03-29 02:23:44 -0400

    Add time annotations

--- slow_fiat_crypto_vm_native_no_op/src/Specific/GF25519Reflective/Reified/AddCoordinatesSlowExample.v
+++ slow_fiat_crypto_vm_native_no_op/src/Specific/GF25519Reflective/Reified/AddCoordinatesSlowExample.v
@@ -1,4 +1,4 @@
 Require Import Crypto.Specific.GF25519Reflective.Reified.AddCoordinates.
 
-Time Definition radd_coordinatesW_pkg' := Eval vm_compute in radd_coordinatesW_pkg.
-Time Definition radd_coordinatesW_pkg'' := Eval native_compute in radd_coordinatesW_pkg.
+Time Definition radd_coordinatesW_pkg' := Eval vm_compute in radd_coordinatesW_pkg. (* 15.087 secs (14.939u,0.151s) *)
+Time Definition radd_coordinatesW_pkg'' := Eval native_compute in radd_coordinatesW_pkg. (* 210.191 secs (15.544u,0.099s) *)
--- slow_fiat_crypto_vm_native_no_op/src/Specific/GF25519Reflective/Reified/LadderStepSlowExample.v
+++ slow_fiat_crypto_vm_native_no_op/src/Specific/GF25519Reflective/Reified/LadderStepSlowExample.v
@@ -1,4 +1,4 @@
 Require Import Crypto.Specific.GF25519Reflective.Reified.LadderStep.
 
-Time Definition rladderstepW_pkg' := Eval vm_compute in rladderstepW_pkg.
-Time Definition rladderstepW_pkg'' := Eval native_compute in rladderstepW_pkg.
+Time Definition rladderstepW_pkg' := Eval vm_compute in rladderstepW_pkg. (* 17.662 secs (17.54u,0.127s) *)
+Time Definition rladderstepW_pkg'' := Eval native_compute in rladderstepW_pkg. (* 231.37 secs (16.58u,0.103s) *)

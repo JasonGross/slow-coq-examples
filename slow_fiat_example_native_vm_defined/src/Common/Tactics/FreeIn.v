1bef1a52b400c87fbbce51bae41364fae7c29393
Jason Gross <jgross@mit.edu>
2017-05-22 18:09:34 -0400

    Remove non-example

--- slow_fiat_example_native_vm_defined/src/Common/Tactics/FreeIn.v
+++
@@ -1,6 +0,0 @@
-Ltac free_in x y :=
-  idtac;
-  match y with
-  | appcontext[x] => fail 1 x "appears in" y
-  | _ => idtac
-  end.
--- slow_fiat_example_native_vm_defined/src/Common/Tactics/IsClosed.v
+++
@@ -1,2 +0,0 @@
-(** Test that a term is ground/closed *)
-Ltac is_closed x := let test := constr:(x) in idtac.
